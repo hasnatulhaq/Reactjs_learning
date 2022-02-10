af155df61038f324e61a04b2cc717a6b50236995
Hasnat ul haq <hasnatulhaq@outlook.com>
2022-02-10 12:48:52 +0500

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Router>
+      <a href='/home'>Go to home page</a>
+      <Routes>
+          <Route path='/'></Route>
+      </Routes>
+    </Router>
   );
 }
 
