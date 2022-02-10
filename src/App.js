3d7bd4df610b174f92dd331bfd05ac2046b3b92e
Hasnat ul haq <hasnatulhaq@outlook.com>
2022-02-10 17:01:14 +0500

    components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './Slice/Home';
+import Login from './Slice/Login';
+import Sigin from './Slice/Sigin';
+import About from './Slice/About';
+
 
 function App() {
   return (
     <Router>
-      <a href='/home'>Go to home page</a>
       <Routes>
-          <Route path='/'></Route>
+          <Route path='/Home' element={<Home/>}></Route>
+          <Route path='/About' element={<About/>}></Route>
+          <Route path='/Login' element={<Login/>}></Route>
+          <Route path='/Signin' element={<Sigin/>}></Route>
       </Routes>
     </Router>
   );
