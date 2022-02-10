1aff7639517ff573550ab3b4b64786126d1e7c0e
Hasnat ul haq <hasnatulhaq@outlook.com>
2022-02-10 10:24:14 +0500

    react router

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>React native</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
